e55e533cddeadee226b1ae390369a4fcb51737ef
yongjin <jyj1641@gmail.com>
2019-09-16 14:58:02 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -1,5 +1,8 @@
+//for command line git tutorial
+//local reposiitory modification
 print("Hello world")
 print("Tell Your World")
 print("Tell his world")
 print("Tell her world")
 print("Tell boy world")
+
